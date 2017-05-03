394cbaee81f3c7e189ce893de2d9b041498cabb7
Sten Turpin <sten@redhat.com>
2017-05-03 13:45:22 -0500

    add a script and a playbook to make adding new hosts to management easier

---
+++ bootstrap.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Get Ansible installed so we can that it can manage this host
+
+# Get OS + version so we know how to install Ansible + deps
+function whathosttypeami { 
+  ostype=""
+  osvers=""
+  if [ -f /etc/fedora-release ]; then 
+    ostype="fedora"
+    osvers=$(rpm -q --qf "%{VERSION}" fedora-release)
+  fi
+  if [ -f /etc/centos-release ]; then    
+    ostype="centos"
+    osvers=$(rpm -q --qf "%{VERSION}" centos-release)
+  fi 
+  if [ -f /etc/redhat-release -a ! -f /etc/fedora-release ]; then
+    ostype="rhel"
+    osvers=$(rpm -q --qf "%{VERSION}" redhat-release)
+  fi
+  ## debian, rasbian, what else? 
+  export ostype osvers
+} 
+
+whathosttypeami
+
+# Install Ansible using system package manager, with whatever deps we'll need right away
+function installansiblewithdeps { 
+  case $ostype in
+  fedora)
+    if [ $osvers -ge 22 ]; then
+      ## whoever decided to rename yum to dnf can go dnf themselves
+      dnf -y install git ansible python-dnf libselinux-python
+    else
+      yum -y install git ansible python-yum
+    fi 
+    ;;
+  centos|rhel)
+    yum -y install git ansible 
+    ;;
+  *)
+    echo "this host type is not added yet"
+   ;;
+  esac
+}
+
+installansiblewithdeps
+
+# Grab my ansible-pull repo and set up config management
+if [ -f $(which git) -a -f $(which ansible) ]; then
+  pushd /tmp
+  git clone https://github.com/stenwt/ansible-pull-managed
+  cd ansible-pull-managed
+  ansible-playbook manage-me.yml
+else
+  echo "Something failed, not going further"
+fi
